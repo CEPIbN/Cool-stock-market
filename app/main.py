5e2d8a9b717de631b163a590233cbd45c26d574c
Ярослав Богачев <bogachev762@gmail.com>
2025-04-05 17:54:52 +0500

    Add new route

--- app/main.py
+++ app/main.py
@@ -6,5 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    html_content = "<h2>Hello METANIT.COM!</h2>"
+    html_content = "<h2>Hello from FastAPI!</h2>"
+    return HTMLResponse(content=html_content)
+
+@app.get("/{item}")
+def read_root_item(item: int):
+    html_content = f"<h2>Ты ввел {item} в path</h2>"
     return HTMLResponse(content=html_content)
