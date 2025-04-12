bd4f6a1e109b1bf2bdcb49f29693dbf3ea4eccea
CEPIbN <cepibn.grey@gmail.com>
2025-04-12 20:14:48 +0500

    Теперь к кластеру можно подключиться с линукса. Нужно добавить несколько хостов в разных подсетях

--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -1,52 +1,41 @@
-# 1. Создание группы безопастности
-resource "yandex_vpc_security_group" "pgsql-sg" {
-  name       = "pgsql-sg"
-  network_id = yandex_vpc_network.project-net.id
-
-  ingress {
-    description    = "PostgreSQL"
-    port           = 6432
-    protocol       = "TCP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# 2. Создание кластер PostgreSQL
-resource "yandex_mdb_postgresql_cluster" "project-pg" {
-  name                = "project-pg"
+resource "yandex_mdb_postgresql_cluster" "project-db-cluster" {
+  name                = "project-db-cluster"
   environment         = "PRODUCTION"
   network_id          = yandex_vpc_network.project-net.id
-  security_group_ids  = [yandex_vpc_security_group.pgsql-sg.id]
   deletion_protection = false
 
   config {
     version = 17
     resources {
-      resource_preset_id = "s2.micro"
+      resource_preset_id = "c3-c2-m4"
       disk_type_id       = "network-ssd"
       disk_size          = 50
+    }
+    pooler_config {
+      pool_discard = false
     }
   }
 
   host {
-    zone      = "ru-central1-d"
-    name      = "project-pg-host-d"
-    subnet_id = yandex_vpc_subnet.project-subnet-d.id
+    zone             = "ru-central1-d"
+    name             = "project-host-db"
+    subnet_id        = yandex_vpc_subnet.project-subnet-d.id
+    assign_public_ip = true
   }
 }
 
-# 3. Создание пользователя
-resource "yandex_mdb_postgresql_user" "project" {
-  cluster_id = yandex_mdb_postgresql_cluster.project-pg.id
-  name       = "project"
-  password   = "MySecurePassword123!"
-  depends_on = [yandex_mdb_postgresql_cluster.project-pg]
+resource "yandex_mdb_postgresql_user" "project-db-user" {
+  cluster_id = yandex_mdb_postgresql_cluster.project-db-cluster.id
+  name       = "project-db-user"
+  password   = "ThisIsSecretPassword321!"
 }
 
-# 4. Создание БД 
-resource "yandex_mdb_postgresql_database" "database" {
-  cluster_id = yandex_mdb_postgresql_cluster.project-pg.id
-  name       = "project-db"
-  owner      = yandex_mdb_postgresql_user.project.name
-  depends_on = [yandex_mdb_postgresql_user.project]
+resource "yandex_mdb_postgresql_database" "project-database" {
+  cluster_id = yandex_mdb_postgresql_cluster.project-db-cluster.id
+  name       = "project-database"
+  owner      = "project-db-user"
+  depends_on = [
+    yandex_mdb_postgresql_user.project-db-user
+  ]
 }
+
