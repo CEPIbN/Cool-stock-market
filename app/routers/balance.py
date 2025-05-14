019e14bac859a7cf9b004d9425dee86a8fdfcb93
Ярослав Богачев <bogachev762@gmail.com>
2025-05-14 12:46:18 +0500

    Add balance endpoint

--- app/routers/balance.py
+++ app/routers/balance.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+
+from app.db import get_db
+from app.middlewares import get_current_user_from_token
+from app.models.models import User, Balance
 
 router_balance = APIRouter(
     prefix="/api/v1/balance",
@@ -11,8 +17,11 @@ router_admin_balance = APIRouter(
 )
 
 @router_balance.get("/")
-def get_balance():
-    return
+def get_balance(current_user: User = Depends(get_current_user_from_token),
+                db: Session = Depends(get_db)):
+    balances = db.query(Balance).filter(current_user.id == Balance.user_id)
+    balances_dict = {i.ticker: i.amount for i in balances}
+    return JSONResponse(content=balances_dict, status_code=200)
 
 @router_admin_balance.post("/deposit")
 def deposit_balance():
