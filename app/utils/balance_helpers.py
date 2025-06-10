9fde676bd9dc1a8519681821d228a85e287a1336
Ярослав Богачев <bogachev762@gmail.com>
2025-06-10 10:07:24 +0500

    fix5

--- app/utils/balance_helpers.py
+++ app/utils/balance_helpers.py
@@ -5,6 +5,7 @@ from app.DTO.Request.CreateOrderBody import OrderBody
 from app.exceptions import CustomAPIException
 from app.models.enums.Direction import Direction
 from app.models.enums.ErrorType import ErrorType
+from app.models.enums.OrderStatus import OrderStatus
 from app.models.models import Balance, BaseOrder, MarketOrder, LimitOrder, Transaction
 
 
@@ -72,8 +73,6 @@ def ensure_balances_exist(db: Session, user_id: UUID, tickers: list[str]):
         if not balance:
             db.add(Balance(user_id=user_id, ticker=ticker))
             created = True
-    # if created:
-    #     db.commit()
 
 def validate_balance(db : Session, order_body : OrderBody, user_id : UUID) -> [Balance, Balance, int]:
     ensure_balances_exist(db, user_id, [order_body.ticker, 'RUB'])
@@ -117,7 +116,7 @@ def estimate_market_order_rate(order_body : OrderBody, db: Session) -> int:
         .filter(LimitOrder.ticker == order_body.ticker,
                 LimitOrder.direction == answer_direction,
                 (LimitOrder.qty - LimitOrder.filled) >= order_body.qty,
-                LimitOrder.status.in_(["NEW", "PARTIALLY_EXECUTED"]))
+                LimitOrder.status.in_([OrderStatus.NEW, OrderStatus.PARTIALLY_EXECUTED]))
         .order_by(LimitOrder.price.asc() if is_buy else LimitOrder.price.desc())
         .first()
     )
